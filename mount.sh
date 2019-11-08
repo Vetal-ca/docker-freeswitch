52d69ad7a7863fb60c9a2a3c377b026300e25500
Vitali Khlebko <vitali@razitall.com>
2019-11-08 18:23:29 -0500

    Overlay mount attempt

--- mount.sh
+++ mount.sh
@@ -3,7 +3,7 @@
 
 usage ()
 {
-    echo "Usage: ${0##*/} mount_dir mount_to_dir"
+    echo "Usage: ${0##*/} mount_dir mount_dir1 mount_to_dir"
     exit 1
 }
 
@@ -18,13 +18,20 @@ fi
 if [ -z "$2" ]; then
   usage
 else
-  mount_to_dir=$2
+  mount_dir1=$2
+fi
+
+if [ -z "$3" ]; then
+  usage
+else
+  mount_to_dir=$3
 fi
 
 mkdir "${mount_to_dir}"
+mkdir "${mount_dir1}"
 
 echo "Mounting \"${mount_dir}\" to \"${mount_to_dir}\""
-mount  "${mount_dir}" "${mount_to_dir}"
+mount -t overlay overlay -o lowerdir="${mount_dir}:${mount_dir1}" "${mount_to_dir}"
 
 echo "Waiting for TERM signal . . ."
 tail -fn0 $0 & PID=$!
