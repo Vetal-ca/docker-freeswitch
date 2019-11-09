12057af839a310389ada9fe8ba04b630b3f4ef83
Vitali Khlebko <vitali@razitall.com>
2019-11-08 19:05:59 -0500

    Sounds-ru, sound files are copied instead of mount tricks

--- copy_data.sh
+++ copy_data.sh
@@ -1,44 +1,14 @@
 #!/usr/bin/env sh
 
 
-usage ()
-{
-    echo "Usage: ${0##*/} mount_dir mount_dir1 mount_to_dir"
-    exit 1
-}
+cp -R /usr/share/freeswitch/sounds/* /sounds
 
-
-
-if [ -z "$1" ]; then
-  usage
-else
-  mount_dir=$1
-fi
-
-if [ -z "$2" ]; then
-  usage
-else
-  mount_dir1=$2
-fi
-
-if [ -z "$3" ]; then
-  usage
-else
-  mount_to_dir=$3
-fi
-
-mkdir "${mount_to_dir}"
-mkdir "${mount_dir1}"
-
-echo "Mounting \"${mount_dir}\" to \"${mount_to_dir}\""
-mount -t overlay overlay -o lowerdir="${mount_dir}:${mount_dir1}" "${mount_to_dir}"
-
+# Wait after copy, so container is not restarted
 echo "Waiting for TERM signal . . ."
 tail -fn0 $0 & PID=$!
 trap "kill $PID" INT TERM
 
 wait
 
-umount  "${mount_to_dir}"
 
 echo "TERM signal is recieved, exit"
