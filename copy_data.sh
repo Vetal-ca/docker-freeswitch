2ada239d264367915c5bde60fb60dbe2435d7a56
Vitali Khlebko <vitali@razitall.com>
2019-11-08 19:11:49 -0500

    All sound files, Dockerfiles are updated to do a data copy

--- copy_data.sh
+++ copy_data.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 
-
+echo "Copying sound files ..."
 cp -R /usr/share/freeswitch/sounds/* /sounds
+echo "Copy is done"
+
 
 # Wait after copy, so container is not restarted
 echo "Waiting for TERM signal . . ."
